ff87b7e31822b06e3562a1f63e17610240b7bf49
shiyi <shiyi25928988@163.com>
2020-12-08 15:31:39 +0800

    20201208 - 1531

--- src/test/java/spring/test/MinioTest.java
+++ src/test/java/spring/test/MinioTest.java
@@ -1,12 +1,19 @@
 package spring.test;
 
+import io.minio.errors.*;
 import minio.client.spring.MinioConfiguration;
 import minio.client.spring.services.FileUploadService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 
 /**
  * @program: minio-client
@@ -23,9 +30,30 @@ public class MinioTest {
 
     @Test
     public void contextLoads() {
-
-
-        System.out.println("111");
+        ApplicationContext ctx = new AnnotationConfigApplicationContext(MinioConfiguration.class);
+        FileUploadService fileUploadService = ctx.getBean(FileUploadService.class);
+        try {
+            fileUploadService.upload("D:\\Downloads\\minio.exe");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (InvalidResponseException e) {
+            e.printStackTrace();
+        } catch (InsufficientDataException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (ServerException e) {
+            e.printStackTrace();
+        } catch (InternalException e) {
+            e.printStackTrace();
+        } catch (XmlParserException e) {
+            e.printStackTrace();
+        } catch (ErrorResponseException e) {
+            e.printStackTrace();
+        }
+        System.out.println("done");
     }
 
 }
